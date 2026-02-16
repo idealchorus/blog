2dd8269784cb642d8c75ccde6944ede66c3a8213
gopherchucks <dusotcharles@gmail.com>
2026-02-16 14:26:26 -0700

    Adding vercel react router plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import deno from "@deno/vite-plugin"
 import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default defineConfig({
-  plugins: [deno(), tailwindcss(), reactRouter()],
+  plugins: [deno(), tailwindcss(), reactRouter(), vercelPreset()],
 })
