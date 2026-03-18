aff190a70b2ccc979a5942e6dca6cebfd15fc290
gopherchucks <dusotcharles@gmail.com>
2026-03-17 21:25:09 -0700

    Working on adding highlighting and a custom theme

--- app/components/fence.tsx
+++ app/components/fence.tsx
@@ -1,9 +1,21 @@
-import { type ComponentPropsWithoutRef } from "react"
+"use client"
+import { useLayoutEffect, type ComponentPropsWithoutRef } from "react"
+import hljs from "highlight.js"
 
 type Props = ComponentPropsWithoutRef<"code"> & {
   language: string
 }
 
-export default function Fence({ language: _language, children, ...rest }: Props) {
-  return <code {...rest}>{children}</code>
+export default function Fence({ language, children, ...rest }: Props) {
+  useLayoutEffect(() => {
+    hljs.highlightAll()
+  }, [])
+
+  return (
+    <pre>
+      <code className={`language-${language}`} {...rest}>
+        {children}
+      </code>
+    </pre>
+  )
 }
